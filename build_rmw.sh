30632c92e279456845df25a5395eca6a850c1f0b
Tyler Mayoff <tyler@tylermayoff.com>
2023-12-16 16:27:21 -0500

    Update repos

--- build_rmw.sh
+++ build_rmw.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . /opt/ros/$ROS_DISTRO/setup.bash
-colcon build --symlink-install --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_opendds_cpp
+colcon build --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_opendds_cpp
