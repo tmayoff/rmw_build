c08fb5858fd6b7dffebb0fefc940f5b9e4eedad4
Tyler <tyler@tylermayoff.com>
2024-01-18 13:29:18 -0500

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -6,4 +6,4 @@ export PATH=/usr/lib/ccache:$PATH
 echo "BUILD ALL"
 ./rmw_build/build_rmw.sh
 . install/local_setup.bash
-colcon --log-level DEBUG build --event-handlers log_command+ --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber examples_rclcpp_minimal_service examples_rclcpp_minimal_client
+VERBOSE=1 colcon --log-level DEBUG build --event-handlers log_command+ --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber examples_rclcpp_minimal_service examples_rclcpp_minimal_client
